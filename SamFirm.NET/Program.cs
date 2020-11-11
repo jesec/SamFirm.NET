c987b0f9725257ae1874db6850bca5b2b0fc0a10
Jesse Chan <jc@linux.com>
2020-11-11 18:29:06 +0800

    Initial commit

---
+++ SamFirm.NET/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SamFirm.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
